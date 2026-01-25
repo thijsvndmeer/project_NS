f6c9edbd991a955e586bbb89faa909608e9bc87d
GreenDestroyer05 <130653638+GreenDestroyer05@users.noreply.github.com>
2026-01-25 21:27:14 +0100

    sdfhk

---
+++ main.py
@@ -0,0 +1,45 @@
+import NS as ns
+from datetime import datetime
+
+
+db = ns.openconnection()
+
+stations = ns.get_stations(db, ['Leiden Centraal', 'Amsterdam Centraal', 'Beverwijk', 'Zoetermeer', 'Hoorn'])
+
+collection_time = int(input('How long do you wish to collect? (in minutes)'))
+
+def main_loop(stations, db):
+
+    for station in stations:
+        now = datetime.now()
+
+        print(f'searching for departures from {station}')
+        departures = ns.get_departures(station, now, stations[station]['iucCode'])
+
+        if len(departures) > 0:
+            ns.insert_stops(departures, db, stations[station]['id'])
+        else: print(f'no departures for {station}')
+
+
+n = 1
+
+while n < collection_time:
+    print(" ")
+    print(" ")
+    print("waiting until next measuring moment...")
+    time = datetime.now().replace(second=0, microsecond=0)
+    t = True
+    while t:
+        if (datetime.now().replace(second=0, microsecond=0) - time).total_seconds() > 0:
+            t = False
+    print(datetime.now().replace(second=0, microsecond=0))
+
+    main_loop(stations, db)
+
+    n += 1
+
+print(' ')
+print(' ')
+print(' ')
+print('Measuring complete')
+print('closing program')
